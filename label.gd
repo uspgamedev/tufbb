3fe88cd633cd8a4d4cff202e0903db908a730cb5
Victor Domiciano <victor.dm3@gmail.com>
2015-10-29 16:58:09 -0200

    Instanciando os estágios via código

--- label.gd
+++ label.gd
@@ -3,9 +3,7 @@ extends Label
 
 func _ready():
 	set_fixed_process(true)
-	
-	
-	
-	
+
 func _fixed_process(delta):
 	pass
+	
--- main.gd
+++ main.gd
@@ -2,9 +2,14 @@
 extends Node
 
 var bricks
+var scene = preload("res://stage-1.xscn")
+var node
 
 func _ready():
 	set_fixed_process(true)
+	
+	node = scene.instance()
+	add_child(node)
 	
 func _fixed_process(delta):
 	if(Input.is_action_pressed("ui_cancel")):
@@ -19,10 +24,10 @@ func _fixed_process(delta):
 	#else:
 	#	print ("umpressed")
 	
-	
 func brickHasDied():
 	var brick1 = get_tree().get_nodes_in_group("brick_1hit").size()
 	var brick2 = get_tree().get_nodes_in_group("brick_2hit").size()
 	var brick3 = get_tree().get_nodes_in_group("brick_3hit").size()
 	
 	bricks = brick1 + 2*brick2 + 3*brick3
+	
