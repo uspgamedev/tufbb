403ec94bf1297848504531a4f238c0d4e5d38ee5
Victor Domiciano <victor.dm3@gmail.com>
2015-12-03 15:02:54 -0200

    Escalando a tela (fullscreen).

--- global.gd
+++ global.gd
@@ -7,13 +7,14 @@ var score
 var placar = File.new()
 
 func _init():
+	OS.set_window_fullscreen(true)
 	placar.open(file_name, File.WRITE)
 	placar.store_32(0)
 	placar.close()
 	
 func next_scene():
 	if (stage == 1):
-		return load("res://stage-2.xscn")
+		return load("res://stage-1.xscn")
 	elif (stage == 2):
 		return load("res://stage-2.xscn")
 	elif (stage == 3):
