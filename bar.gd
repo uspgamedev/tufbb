5bc0932f3712a2abe423d34df7420a26a9ea9442
Victor Domiciano <victor.dm3@gmail.com>
2015-12-04 15:16:35 -0200

    Correção da barra surgindo nas margens.

--- bar.gd
+++ bar.gd
@@ -10,8 +10,7 @@ var deltavel = 0
 func _ready():
 	set_fixed_process(true)
 	
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	var tween = get_node("../Tween")
 	var posicao = get_pos()
 	
@@ -44,13 +43,3 @@ func _fixed_process(delta):
 	if (get_viewport().get_mouse_pos().x > rect.end.x - tamanho.x/2):
 		set_pos (Vector2 (rect.end.x - tamanho.x/2, posicao.y))
 	
-	#correcao do mouse saindo da tela:
-	
-	var stretch = OS.get_video_mode_size().y / get_viewport().get_rect().end.y
-	var black = (OS.get_video_mode_size().x - stretch*get_viewport().get_rect().end.x) / 2
-	
-	if (get_viewport().get_mouse_pos().x < rect.pos.x + tamanho.x/2):
-		Input.warp_mouse_pos(Vector2(rect.pos.x + black + stretch*tamanho.x/2, OS.get_video_mode_size().y/2))
-	
-	if (get_viewport().get_mouse_pos().x > rect.end.x - tamanho.x/2):
-		Input.warp_mouse_pos(Vector2(OS.get_video_mode_size().x - stretch*tamanho.x/2 - black, OS.get_video_mode_size().y/2))
--- main.gd
+++ main.gd
@@ -7,6 +7,8 @@ var points
 var file_name = "res://placar"
 var score
 var placar = File.new()
+var stretch
+var black
 
 func _ready():
 	set_process_input(true)
@@ -31,10 +33,23 @@ func _ready():
 	
 	bricks = brick1 + 2*brick2 + 3*brick3
 	
+	stretch = OS.get_video_mode_size().y / get_viewport().get_rect().end.y
+	black = (OS.get_video_mode_size().x - stretch*get_viewport().get_rect().end.x) / 2
+	
 func _fixed_process(delta):
 	
 	var label = get_node("Label")
 	label.set_text(str("Score: ", score))
+	
+	#correcao do mouse saindo da tela:
+	
+	var bar = get_node("bar")
+	
+	if (get_viewport().get_mouse_pos().x < get_viewport().get_rect().pos.x + bar.tamanho.x/2):
+		Input.warp_mouse_pos(Vector2(get_viewport().get_rect().pos.x + black + stretch*bar.tamanho.x/2, OS.get_video_mode_size().y/2))
+	
+	if (get_viewport().get_mouse_pos().x > get_viewport().get_rect().end.x - bar.tamanho.x/2):
+		Input.warp_mouse_pos(Vector2(OS.get_video_mode_size().x - stretch*bar.tamanho.x/2 - black, OS.get_video_mode_size().y/2))
 	
 func _input(event):
 	if (event.is_action("ui_cancel") and !event.is_pressed()):
@@ -44,13 +59,12 @@ func _input(event):
 			bar.set_fixed_process(false)
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else:
-			var stretch = OS.get_video_mode_size().y / get_viewport().get_rect().end.y
-			var black = (OS.get_video_mode_size().x - stretch*get_viewport().get_rect().end.x) / 2
+			var bar_pos = bar.get_pos()
 			get_tree().set_pause(false)
 			bar.set_fixed_process(true)
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-			Input.warp_mouse_pos(Vector2(black + stretch*bar.get_pos().x, stretch*bar.get_pos().y))
-			
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+			Input.warp_mouse_pos(Vector2(black + stretch*bar_pos.x, stretch*bar_pos.y))
+			bar.set_pos(Vector2(black + stretch*bar_pos.x, bar_pos.y))
 	
 func brickHasDied(points):
 	var brick1 = get_tree().get_nodes_in_group("brick_1hit").size()
