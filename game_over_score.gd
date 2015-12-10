3f1ef9cac54cf3383942980c08cd5da1971a2fc1
Victor Domiciano <victor.dm3@gmail.com>
2015-12-10 15:50:01 -0200

    Corrigindo o alinhamento do placar.

--- game_over_score.gd
+++ game_over_score.gd
@@ -2,7 +2,6 @@
 extends Label
 var file_name = "res://placar"
 var score
-var tmp
 var cont = 0
 var placar = File.new()
 
@@ -10,11 +9,4 @@ func _ready():
 	placar.open(file_name, File.READ)
 	score = placar.get_32()
 	placar.close()
-	tmp = score
-	while (tmp / 10 != 0):
-		tmp = tmp / 10
-		cont += 1
-	
-	self.set_pos(Vector2(370 - cont*12, self.get_pos().y))
-	
 	set_text(str(score))
